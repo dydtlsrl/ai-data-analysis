775ab495b5a61256a88713ccd0e0a90ff2bd832c
양성용 <dydtlsrl@gmail.com>
2026-08-25 18:27:19 +0900

    20260825

---
+++ python-basics/chapter11/EX/lever_02.py
@@ -0,0 +1,18 @@
+# LEVEL_02 리스트를 한 줄씩 수정하고 상태를 추적하라.
+
+scores = [70, 80, 90]
+scores[0] = 75 #[75,80, 90]
+scores[2] = 95 #[75, 80, 95]
+scores[1] = 85 #[75, 85,95]
+
+print(scores)
+
+
+#추가 Challenge
+
+foods =["김밥","라면","떡볶이","우동"]
+
+foods[1] = "파스타"
+foods[3] = "냉면"
+
+print(foods)
---
+++ python-basics/chapter13/00.py
@@ -0,0 +1,3 @@
+profile = {}
+
+print(type(profile))
